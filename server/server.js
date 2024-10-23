3aac95088ea3a5bdaadb1afc8065c1537e746cea
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-10-23 16:53:01 +0530

    backed server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./lib/db");
 const authRoute = require('./routes/authRoute')
+const productRoute = require('./routes/productRoute')
+const cartRoute = require('./routes/cartRoute')
+const couponRoute = require('./routes/couponRoute')
+const analyticsRoute = require('./routes/analyticsRoute')
 const PORT = process.env.PORT || 5000;
 
 // db connection
@@ -17,6 +21,10 @@ app.use(cookieParser());
 
 // api endpoints
 app.use('/api/auth', authRoute)
+app.use('/api/product', productRoute)
+app.use('/api/cart', cartRoute)
+app.use('/api/coupon', couponRoute)
+app.use('/api/analytics', analyticsRoute)
 
 // server
 app.listen(PORT, () => {
